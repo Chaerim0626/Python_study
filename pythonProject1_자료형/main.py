ed32b66f58d8ead4b12c2c1e81e0bfbcf1fafa1a
Chaerim Park <dasapcr@gmail.com>
2021-11-30 21:51:36 +0900

    commit

--- pythonProject1_자료형/main.py
+++ pythonProject1_자료형/main.py
@@ -1,5 +1,5 @@
 #자료형
-#숫자형
+#1. 숫자형
 a = 123
 a = 1.2
 a = 0o177 #8진수 (0o or 0O로 시작)
@@ -19,7 +19,7 @@ print (c ** d) #제곱 연산자
 
 
 
-#문자열 자료형
+#2. 문자열 자료형
 say = "\"Python is very easy.\" he says." #큰따옴표, 작은따옴표를 문자로 쓰고 싶을때는 앞에 백슬래쉬 쓰기
 
 #개행 ('''나 """)
@@ -64,3 +64,40 @@ word.rstrip() #오른쪽 공백 지우기
 word.strip() #양쪽 공백 지우기
 word.replace("h", "o") #문자열 바꾸기
 word.split() #문자열 공백기준으로 나누기
+
+#고급 문자열 포맷팅
+print("I eat {0} apples.".format(3)) #숫자 바로 대입하기
+print("I eat {0} apples.".format("five")) #문자열 바로 대입하기
+number = 3
+print("I eat {0} apples.".format(number)) #숫자값을 가진 변수로 대입하기
+day = "five"
+print("I ate {0} apples. so I was sick for {1} days.".format(number,day)) #2개이상의 값 넣기
+
+
+#3. 리스트 자료형
+odd = [1, 3, 5, 7, 9] #리스트 형식
+a = [1, 2, 3, 4, 5]
+print(a[0:2]) #리스트 슬라이싱은 문자열 슬라이싱과 동일
+print(odd + a) #리스트 더하기
+print(a*3) #리스트 반복하기
+
+#리스트의 수정, 변경, 삭제
+a[2] = 4
+a[1:2] = ['a', 'b', 'c']
+a[1:3] = []
+del a[1]
+
+#리스트 관련 함수들
+b = [1, 2, 3]
+b.append([4, 5]) #리스트 마지막에 추가
+b.reverse() #리스트 뒤집기
+b.index(1) #위치 반환
+b.insert(0,4) #리스트에 요소 삽입
+b.remove(1) #첫번째로 나오는 1을 제거
+b.pop() #리스트의 맨 마지막 요소 꺼내 삭제
+b.pop(1) #리스트의 1번째 요소 꺼내 삭제
+b.count(2) #리스트에 2가 몇개 있는지 세기
+b.sort() #리스트 정렬
+c = [6,7]
+b.extend(c) #b에 c리스트 더하기
+print(b)
